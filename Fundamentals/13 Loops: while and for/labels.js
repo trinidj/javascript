b57a0ea1682100c704e9676fbc5cc52191af3421
trinidj <jm.trinidad199@gmail.com>
2025-06-03 19:48:35 -0400

    added continue and labels

---
+++ Fundamentals/13 Loops: while and for/labels.js
@@ -0,0 +1,9 @@
+outer: for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    let input = prompt(`Value at coords (${i},${j})`, '');  
+
+    if (!input) break outer;
+  }
+}
+
+console.log('Done!');
