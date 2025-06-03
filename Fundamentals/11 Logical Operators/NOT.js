fbbb8c7e34377adb69e2c971b47c921c5c63ce1c
trinidj <jm.trinidad199@gmail.com>
2025-06-02 22:35:06 -0400

    added AND and NOT files

---
+++ Fundamentals/11 Logical Operators/NOT.js
@@ -0,0 +1,9 @@
+console.log(!true); //* false
+console.log(!0);    //* true
+
+console.log(!!"non-empty string"); //* true
+console.log(!!null); //* false
+
+/*
+  The first NOT converts the value into a boolean, and returns the inverse. 
+*/
