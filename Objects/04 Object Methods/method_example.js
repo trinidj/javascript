ffe5a154a27240d7656c9a7c5d677f3c3560aa19
trinidj <jm.trinidad199@gmail.com>
2025-06-09 18:49:51 -0400

    added object method example

---
+++ Objects/04 Object Methods/method_example.js
@@ -0,0 +1,26 @@
+let user = {
+  name: "John",
+  age: 21,
+};
+
+user.sayHi = function() {
+  console.log("Hello!");
+};
+
+user.sayHi();
+
+/*
+  Same as:
+
+  let user = {
+    ...
+  }
+
+  function sayHi() {
+    console.log("Hello!");
+  }
+
+  user.sayHi = sayHi;
+
+  user.sayHi();
+*/
